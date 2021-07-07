15bc48b57f185a9213ee84c3d32f549d0b920fac
kanak gautam <kanakgautam5@gmail.com>
2021-07-07 22:25:26 +0530

    added another files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from "react";
+import Todo from "./todo"
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value
+    setInputText(newValue);
+  }
+
+  function handleClick() {
+    setItems((prevValue) => {
+      return [...prevValue, inputText];
+    })
+    setInputText("");
+  }
+
+  function deleteItem(id) {
+    setItems((prevValue) => {
+      return prevValue.filter((element, index) => {
+        return index !== id;
+      })
+    })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="heading">Todo-List</h1>
+      <div className="form">
+        <input onChange={handleChange} type="text" value={inputText}></input>
+        <button onClick={handleClick} className="button-input">Add</button>
+      </div>
+      <div>
+        <ul className="list">
+          {items.map((element, index) => {
+            return <Todo value={element} key={index} pos={index} deleteItem={deleteItem} />
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
